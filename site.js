5647673021a2bdfff06baa601ee1d40c04d85d53
Sean Kelliher <sbkelliher@icloud.com>
2020-04-17 13:35:16 -0400

    Add showCelicus and hideCelicus functions.

--- site.js
+++ site.js
@@ -193,4 +193,32 @@ weather = {
         });
     },
 
+    //Show Celcius.
+    showCelcius: function () {
+        "use strict";
+
+        //Gather the Celcius elements. Turn into real array.
+        const celcis = document.getElementsByClassName("celci");
+        const celcisArray = Array.from(celcis);
+
+        //For each loop, set display to inline.
+        celcisArray.forEach(function (celci) {
+            celci.style.display = "inline";
+        });
+    },
+
+    //Hide Celcius.
+    hideCelcius: function () {
+        "use strict";
+
+        //Gather the Celcius elements. Turn into real array.
+        const celcis = document.getElementsByClassName("celci");
+        const celcisArray = Array.from(celcis);
+
+        //For each loop, set display to none.
+        celcisArray.forEach(function (celci) {
+            celci.style.display = "none";
+        });
+    },
+
 }; //close weatherAll.
