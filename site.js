24eeab59e3b8408671a52b04750c9ed17bed0047
Sean Kelliher <sbkelliher@icloud.com>
2020-04-17 13:33:31 -0400

    Add showFarenheit and hideFarenheit functions.

--- site.js
+++ site.js
@@ -137,4 +137,32 @@ weather = {
         }
     },
 
+    //Show Farenheit.
+    showFarenheit: function () {
+        "use strict";
+
+        //Gather the Farenheit elements. Turn into real array.
+        const farens = document.getElementsByClassName("faren");
+        const farensArray = Array.from(farens);
+
+        //For each loop, set display to inline.
+        farensArray.forEach(function (faren) {
+            faren.style.display = "inline";
+        });
+    },
+
+    //Hide Farenheit.
+    hideFarenheit: function () {
+        "use strict";
+
+        //Gather the Farenheit elements. Turn into real array.
+        const farens = document.getElementsByClassName("faren");
+        const farensArray = Array.from(farens);
+
+        //For each loop, set display to none.
+        farensArray.forEach(function (faren) {
+            faren.style.display = "none";
+        });
+    },
+
 }; //close weatherAll.
