e253f9e52b657f98315a59736fac6dba1d291f4e
Sean Kelliher <sbkelliher@icloud.com>
2020-04-17 13:34:24 -0400

    Add showSlash and hideSlash functions.

--- site.js
+++ site.js
@@ -165,4 +165,32 @@ weather = {
         });
     },
 
+    //Show Slash.
+    showSlash: function () {
+        "use strict";
+
+        //Gather the slashes elements. Turn into real array.
+        const slashes = document.getElementsByClassName("slash");
+        const slashesArray = Array.from(slashes);
+
+        //For each loop, set display to inline.
+        slashesArray.forEach(function (slash) {
+            slash.style.display = "inline";
+        });
+    },
+
+    //Hide Slash.
+    hideSlash: function () {
+        "use strict";
+
+        //Gather the slashes elements. Turn into real array.
+        const slashes = document.getElementsByClassName("slash");
+        const slashesArray = Array.from(slashes);
+
+        //For each loop, set display to none.
+        slashesArray.forEach(function (slash) {
+            slash.style.display = "none";
+        });
+    },
+
 }; //close weatherAll.
