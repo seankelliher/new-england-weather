183bcfe70b1d25ecf057b6e54e2c948e632f31d4
Sean Kelliher <sbkelliher@icloud.com>
2020-04-17 13:18:52 -0400

    Rename global variable (like previous commit). Add 'noaa' array.

--- site.js
+++ site.js
@@ -10,8 +10,49 @@ document.addEventListener("DOMContentLoaded", function () {
     weather.setLocal();
 });
 
-//The weatherAll object.
-weatherAll = {
+//The weather object.
+weather = {
+
+    //Object with weather info.
+    //Note: "capitol" is not necessary. Just here for reference.
+    noaa: [
+        {
+            state: "Connecticut",
+            abbreviation: "ct",
+            capitol: "Hartford",
+            noaaid: "KHFD"
+        },
+        {
+            state: "Maine",
+            abbreviation: "me",
+            capitol: "Augusta",
+            noaaid: "KAUG"
+        },
+        {
+            state: "Massachusetts",
+            abbreviation: "ma",
+            capitol: "Boston",
+            noaaid: "KBOS"
+        },
+        {
+            state: "New Hampshite",
+            abbreviation: "nh",
+            capitol: "Concord",
+            noaaid: "KCON"
+        },
+        {
+            state: "Rhode Island",
+            abbreviation: "ri",
+            capitol: "Providence",
+            noaaid: "KPVD"
+        },
+        {
+            state: "Vermont",
+            abbreviation: "vt",
+            capitol: "Montpelier",
+            noaaid: "KMPV"
+        }
+    ],
 
     //Function for Connecticut weather.
     weatherCt: function () {
