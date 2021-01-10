c43fbf0f1b2a6b3d1cbaf10b587c2444126455be
Sean Kelliher <sbkelliher@icloud.com>
2021-01-10 15:18:58 -0500

    Add 'units.js'. Replaces 'farenheit.js slash.js celcius.js' files.

---
+++ app/units.js
@@ -0,0 +1,29 @@
+//Show units (faren, slash, celci).
+function showUnits(unit) {
+    "use strict";
+
+    //Gather elements. Make "real" array.
+    const units = document.getElementsByClassName(unit);
+    const unitsArray = Array.from(units);
+
+    //Loop. Set display to "inline".
+    unitsArray.forEach(function (item) {
+        item.style.display = "inline";
+    });
+}
+
+//Hide units (faren, slash, celci).
+function hideUnits(unit) {
+    "use strict";
+
+    //Gather elements. Make "real" array.
+    const units = document.getElementsByClassName(unit);
+    const unitsArray = Array.from(units);
+
+    //Loop. Set display to "none".
+    unitsArray.forEach(function (item) {
+        item.style.display = "none";
+    });
+}
+
+export {showUnits, hideUnits};
