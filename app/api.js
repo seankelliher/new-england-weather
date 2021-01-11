08af2fce529ea3dc233336b4b48a95bbb6d802c9
Sean Kelliher <sbkelliher@icloud.com>
2021-01-11 17:18:41 -0500

    Add 'condition' const and arguement.

--- app/api.js
+++ app/api.js
@@ -8,13 +8,9 @@ function fetchWeather(station) {
     fetch(apiUrl).then(function (response) {
         return response.json();
     }).then(function (data) {
-        //const elevation = data.properties.elevation.value;
-        //const elevationUnit = data.properties.elevation.unitCode;
         const temperature = data.properties.temperature.value;
-        //const temperatureUnit = data.properties.temperature.unitCode;
-        //const condition = data.properties.textDescription;
-
-        createElements(station, temperature);
+        const condition = data.properties.textDescription;
+        createElements(station, temperature, condition);
     }).catch(function () {
         createElements(station, "error");
     });
