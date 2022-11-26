7242d83abd135ebdf6147050cd5eab9746c661de
Sean Kelliher <sbkelliher@icloud.com>
2022-11-26 12:31:32 -0500

    Swap h2 and h3.

--- app/elements.js
+++ app/elements.js
@@ -1,96 +1,96 @@
 function createElements(station) {
     "use strict";
 
-    //Get main element.
+    // Get main element.
     const main = document.querySelector("main");
 
-    //Create needed elements.
+    // Create needed elements.
     const section = document.createElement("section");
-    const h3tag = document.createElement("h3");
     const h2tag = document.createElement("h2");
+    const h3tag = document.createElement("h3");
 
-    //Assign id to section.
+    // Assign id to section.
     section.id = station.toLowerCase();
 
-    //Assign content to h2 and h3.
+    // Assign content to h2 and h3.
     switch (station) {
     case "KAUG":
-        h3tag.textContent = "Maine";
-        h2tag.textContent = "Augusta";
+        h2tag.textContent = "Maine";
+        h3tag.textContent = "Augusta";
         break;
     case "KBOS":
-        h3tag.textContent = "Massachusetts";
-        h2tag.textContent = "Boston";
+        h2tag.textContent = "Massachusetts";
+        h3tag.textContent = "Boston";
         break;
     case "KCON":
-        h3tag.textContent = "New Hampshire";
-        h2tag.textContent = "Concord";
+        h2tag.textContent = "New Hampshire";
+        h3tag.textContent = "Concord";
         break;
     case "KHFD":
-        h3tag.textContent = "Connecticut";
-        h2tag.textContent = "Hartford";
+        h2tag.textContent = "Connecticut";
+        h3tag.textContent = "Hartford";
         break;
     case "KPVD":
-        h3tag.textContent = "Rhode Island";
-        h2tag.textContent = "Providence";
+        h2tag.textContent = "Rhode Island";
+        h3tag.textContent = "Providence";
         break;
     case "KMPV":
-        h3tag.textContent = "Vermont";
-        h2tag.textContent = "Montpelier";
+        h2tag.textContent = "Vermont";
+        h3tag.textContent = "Montpelier";
         break;
     default:
-        h3tag.textContent = "Error";
         h2tag.textContent = "Error";
+        h3tag.textContent = "Error";
     }
 
-    //Append elements.
-    section.appendChild(h3tag);
+    // Append elements.
     section.appendChild(h2tag);
+    section.appendChild(h3tag);
     main.appendChild(section);
 }
 
 function completeElements(station, temperature, condition) {
     "use strict";
 
-    //Create needed elements.
+    // Create needed elements.
     const ptag1 = document.createElement("p");
     const ptag2 = document.createElement("p");
     const span1 = document.createElement("span");
     const span2 = document.createElement("span");
     const span3 = document.createElement("span");
 
-    //Assign content depending on API fetch result.
+    // Assign content depending on API fetch result.
     if (temperature === "error") {
         span1.textContent = "not available";
         span3.textContent = "not available";
         ptag2.textContent = "";
     } else {
-        const faren = (temperature * 1.8 + 32); //Convert temperature
-        const farenFixed = faren.toFixed(1); //to Fahrenheit.
+        const faren = (temperature * 1.8 + 32); // Convert temperature
+        const farenFixed = faren.toFixed(1); // to Fahrenheit.
         span1.textContent = `${farenFixed}\u00B0F`;
         span3.textContent = `${temperature}\u00B0C`;
         ptag2.textContent = `${condition}`;
     }
 
-    //Assign content, used in all results.
+    // Assign content, used in all results.
     span2.textContent = " / ";
 
-    //Assign needed class names.
+    // Assign needed class names.
     span1.className = "faren";
     span2.className = "slash";
     span3.className = "celci";
-    ptag2.className = "details"; //for condition like "cloudy".
+    ptag2.className = "details"; // for condition like "cloudy".
 
-    //Append elements.
+    // Append elements.
     ptag1.appendChild(span1);
     ptag1.appendChild(span2);
     ptag1.appendChild(span3);
 
-    //Get section for station being received.
+    // Get section for station being received.
     const sectionId = station.toLowerCase();
     const section = document.getElementById(sectionId);
 
-    //Append more elements.
+    // Append more elements.
     section.appendChild(ptag1);
     section.appendChild(ptag2);
 }
